902cd655ad2f6d78d01c758496d61956a9ed952a
talha.shoaib <talha@yallamotor.com>
2018-03-25 23:24:50 +0500

    remove subdomain restriction

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  constraints subdomain: 'api' do
-	  scope module: 'api' do
-	    namespace :v1 do
-	      resources :todos do
-			    resources :items
-			  end
-	    end
-	  end
-	end
+  scope module: 'api' do
+    namespace :v1 do
+      resources :todos
+    end
+  end
 end
