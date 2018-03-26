458dee6332cc2422afef3a81e6abf26c31ce2311
talha.shoaib <talha@yallamotor.com>
2018-03-26 19:11:15 +0500

    convert to post request

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :todos do
 			  collection do
-			    get 'video_uploaded'
+			    post 'video_uploaded'
         end
       end
     end
